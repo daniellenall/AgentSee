4c4bb6ab99fae07322f1d636d26e25fa9e64fb20
Danielle <daniellernall@gmail.com>
2020-08-17 17:25:52 +0100

    Restrict routes to logged in users

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
+import Signup from '../views/Signup.vue';
 import AgentList from '../views/AgentList.vue';
+import {fb} from '../firebase';
 
 Vue.use(VueRouter)
 
@@ -20,22 +22,40 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
-  { 
-    path: '/login', 
+  {
+    path: '/login',
     name: 'Login',
     component: Login
   },
   {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
     path: '/agents',
     name: 'AgentList',
-    component: AgentList
+    component: AgentList,
+    meta: { requiresAuth: true }
   }
-
-
-]
+];
 
 const router = new VueRouter({
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
+  const currentUser = fb.auth().currentUser
+
+  if (requiresAuth && !currentUser) {
+    next('/login');
+  } else if (requiresAuth && currentUser) {
+    next();
+  } else {
+    next();
+  }
+}
+);
 
 export default router
