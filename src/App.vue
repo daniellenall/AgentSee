796ca78d5e1d2d3d4e105cdb66bd2fdfb317fbfd
Danielle <daniellernall@gmail.com>
2020-08-18 06:30:45 +0100

    Change active link colour to something more readable

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
@@ -37,6 +35,6 @@ export default {
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #0d6b7a;
 }
 </style>
