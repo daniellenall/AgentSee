05fb2909d960ee556c4cdc73013cf9713d692f03
Danielle <daniellernall@gmail.com>
2020-08-18 08:34:33 +0100

    Basic landing page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="landing">
-    <div class="jumbotron jumbotron-fluid">
+    <div class="jumbotron jumbotron-fluid bg-info">
       <div class="container">
         <h1 class="display-4">{{ msg }}</h1>
-        <p
-          class="lead"
-        >This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        <p class="lead">Internal agent management system for Example Ltd.</p>
+      </div>
+    </div>
+
+    <div class="container text-center">
+      <h2>Features</h2>
+
+      <div class="row ">
+        <div class="col-md-4 col-sm-12">
+          <h3>Find Estate Agents</h3>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+        </div>
+        <div class="col-md-4 col-sm-12">
+          <h3>Update Records</h3>
+          <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        </div>
+        <div class="col-md-4 col-sm-12">
+          <h3>Add New Records</h3>
+          <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus</p>
+        </div>
       </div>
     </div>
   </div>
@@ -19,21 +36,3 @@ export default {
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
